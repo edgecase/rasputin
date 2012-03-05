615eed46371f41b5b1ec51c8cb3e962708ef019d
Pairing Machine <pairing@edgecase.com>
2012-03-05 17:03:55 -0500

    Removed Rails dependency.

--- lib/rasputin.rb
+++ lib/rasputin.rb
@@ -10,19 +10,3 @@ require "rasputin/haml" if defined? Haml
 
 require "rasputin/require_preprocessor"
 
-module Rasputin
-  class Engine < ::Rails::Engine
-    config.rasputin = ActiveSupport::OrderedOptions.new
-    config.rasputin.precompile_handlebars = Rails.env.production?
-    config.rasputin.template_name_separator = '/'
-
-    config.rasputin.use_javascript_require = true
-    config.rasputin.strip_javascript_require = true
-
-    initializer :setup_rasputin, :group => :all do |app|
-      app.assets.register_preprocessor 'application/javascript', Rasputin::RequirePreprocessor
-      app.assets.register_engine '.handlebars', Rasputin::HandlebarsTemplate
-      app.assets.register_engine '.hbs', Rasputin::HandlebarsTemplate
-    end
-  end
-end
--- lib/rasputin/handlebars/template.rb
+++ lib/rasputin/handlebars/template.rb
@@ -7,12 +7,8 @@ module Rasputin
     def prepare; end
 
     def evaluate(scope, locals, &block)
-      if Rails.configuration.rasputin.precompile_handlebars
-        func = Rasputin::Handlebars.compile(data)
-        "Ember.TEMPLATES[#{template_path(scope.logical_path).inspect}] = Ember.Handlebars.template(#{func});"
-      else
-        "Ember.TEMPLATES[#{template_path(scope.logical_path).inspect}] = Ember.Handlebars.compile(#{indent(data).inspect});"
-      end
+      func = Rasputin::Handlebars.compile(data)
+      "Ember.TEMPLATES[#{template_path(scope.logical_path).inspect}] = Ember.Handlebars.template(#{func});"
     end
 
     private
@@ -20,7 +16,7 @@ module Rasputin
     def template_path(path)
       path = path.split('/')
       path.delete('templates')
-      path.join(Rails.configuration.rasputin.template_name_separator)
+      path.join('/')
     end
 
     def indent(string)
